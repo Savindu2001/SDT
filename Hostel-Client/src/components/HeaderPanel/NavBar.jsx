814df786776ae3c7ac0e3bcea2370d4cd563081f
Savindu2001 <76149334+Savindu2001@users.noreply.github.com>
2024-03-30 14:35:16 +0530

    header setup and responsive with mobile

--- Hostel-Client/src/components/HeaderPanel/NavBar.jsx
+++ Hostel-Client/src/components/HeaderPanel/NavBar.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Links from '../Links/Links';
+import { Link } from "react-router-dom";
+import { FaTimes } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
 
-function Navbar() {
-
- const logoName = 'GreenUniHomes';
-
-  return (
-    <>
-    <div>
-      <div>
-        <img src="#" alt="GreenUniHome" />
-      </div>
-        <Links/>
-    </div>
-    </>
+const Navbar = () => {
+  const [click,setClick] = useState(false);
+  const handleClick = () =>{
+    setClick(!click);
+  }
+  const logoName = 'GreenUniHomes';
+  const content = <>
+  <div className='absolute left-0 right-0 block w-full transition bg-blue-500 lg:hidden top-16'>
     
-  )
-}
+      <ul className='p-20 text-xl text-center'>
+        <Link spy={true} smooth={true} to="/">
+        <li className='py-4 my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Home</li>
+        </Link>
+        <Link spy={true} smooth={true} to="/About">
+        <li className='py-4 my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>About</li>
+        </Link>
+        <Link spy={true} smooth={true} to="/Hostel">
+        <li className='py-4 my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Hostel</li>
+        </Link>
+        <Link spy={true} smooth={true} to="/Contact">
+        <li className='py-4 my-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded'>Contact</li>
+        </Link>
+      </ul>
+    
+  </div>
+  </>
+  return(
+    <nav className='bg-emerald-800'>
+      <div className='z-50 flex justify-between flex-1 px-20 py-4 text-white h-10vh lg:py-5'>
+        <div className='flex items-center flex-1'>
+          <span className='text-3xl font-bold'>{logoName}</span>
+        </div>
+        <div className='items-center justify-end hidden font-normal flex-2 lg:flex md:flex lg:'>
+          <div className='flex-10'>
+            <Links spy={true} smooth={true} />
+          </div>
+        </div>
+        <div>
+        {click && content}
+        </div>
+        <button className='block transition sm:hidden' onClick={handleClick}>
+          {click ? <FaTimes size={30} /> : <CiMenuFries size={30}/>}
+        </button>
+      </div>
+    </nav>
+  );
+
+};
+
 
 export default Navbar
--- Hostel-Client/src/components/Links/Links.jsx
+++ Hostel-Client/src/components/Links/Links.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import MenuLinks from '../MenuLinks/MenuLinks'
 
-function Links() {
+function Links()  {
   return (
     <>
-    <MenuLinks linkname='Home' url='/' />
-    <MenuLinks linkname='About us' url='/About' />
-    <MenuLinks linkname='Contact' url='/Contact' />
-    <MenuLinks linkname='Hostels' url='/Hostels' />
-    <MenuLinks linkname='Price' url='/Price' />
-    <MenuLinks linkname='Contact us' url='/Contact' />
+    <ul className='flex gap-8 mr-16 text-[18px]'>
+    <li className='border-b-2 cursor-pointer hover:text-blue-500 transit border-emerald-800 hover:border-blue-500'><MenuLinks linkname='Home' url='/' /></li>
+    <li className='border-b-2 cursor-pointer hover:text-blue-500 transit border-emerald-800 hover:border-blue-500'><MenuLinks linkname='About us' url='/About' /></li>
+    <li className='border-b-2 cursor-pointer hover:text-blue-500 transit border-emerald-800 hover:border-blue-500'><MenuLinks linkname='Contact' url='/Contact' /></li>
+    <li className='border-b-2 cursor-pointer hover:text-blue-500 transit border-emerald-800 hover:border-blue-500'><MenuLinks linkname='Hostels' url='/Hostels' /></li>
+    <li className='border-b-2 cursor-pointer hover:text-blue-500 transit border-emerald-800 hover:border-blue-500'><MenuLinks linkname='Price' url='/Price' /></li>
+    <li className='border-b-2 cursor-pointer hover:text-blue-500 transit border-emerald-800 hover:border-blue-500'><MenuLinks linkname='Contact us' url='/Contact' /></li>
+    </ul>
+    
     </>
   )
 }
